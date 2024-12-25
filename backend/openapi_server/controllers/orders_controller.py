7dbc48ac189f62d503d3b50e9472d93437fdb3e4
Mallo321123 <mario@binary-kitchen.de>
2024-12-25 23:56:17 +0100

    openapi changes

--- backend/openapi_server/controllers/orders_controller.py
+++ backend/openapi_server/controllers/orders_controller.py
@@ -3,7 +3,7 @@ from typing import Dict
 from typing import Tuple
 from typing import Union
 
-from openapi_server.models.orders import Orders  # noqa: E501
+from openapi_server.models.orders_add import OrdersAdd  # noqa: E501
 from openapi_server import util
 
 
@@ -30,36 +30,36 @@ def orders_get(limit=None, page=None):  # noqa: E501
     :param page: page number
     :type page: int
 
-    :rtype: Union[List[Orders], Tuple[List[Orders], int], Tuple[List[Orders], int, Dict[str, str]]
+    :rtype: Union[List[OrdersAdd], Tuple[List[OrdersAdd], int], Tuple[List[OrdersAdd], int, Dict[str, str]]
     """
     return 'do some magic!'
 
 
-def orders_post(orders=None):  # noqa: E501
+def orders_post(orders_add=None):  # noqa: E501
     """add an order
 
     adds an order # noqa: E501
 
-    :param orders: 
-    :type orders: dict | bytes
+    :param orders_add: 
+    :type orders_add: dict | bytes
 
     :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
     """
     if connexion.request.is_json:
-        orders = Orders.from_dict(connexion.request.get_json())  # noqa: E501
+        orders_add = OrdersAdd.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
-def orders_put(orders=None):  # noqa: E501
+def orders_put(orders_add=None):  # noqa: E501
     """update an order
 
     updates an order # noqa: E501
 
-    :param orders: 
-    :type orders: dict | bytes
+    :param orders_add: 
+    :type orders_add: dict | bytes
 
     :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
     """
     if connexion.request.is_json:
-        orders = Orders.from_dict(connexion.request.get_json())  # noqa: E501
+        orders_add = OrdersAdd.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
--- backend/openapi_server/models/__init__.py
+++ backend/openapi_server/models/__init__.py
@@ -1,16 +1,26 @@
 # flake8: noqa
 # import models into model package
+from openapi_server.models.created import Created
 from openapi_server.models.dashboard import Dashboard
 from openapi_server.models.groups import Groups
 from openapi_server.models.groups_response import GroupsResponse
+from openapi_server.models.internal_server_error import InternalServerError
+from openapi_server.models.invalid_request import InvalidRequest
 from openapi_server.models.lights import Lights
 from openapi_server.models.lights_response import LightsResponse
 from openapi_server.models.map import Map
-from openapi_server.models.orders import Orders
+from openapi_server.models.not_found import NotFound
+from openapi_server.models.orders_add import OrdersAdd
+from openapi_server.models.orders_change import OrdersChange
+from openapi_server.models.orders_response import OrdersResponse
 from openapi_server.models.products import Products
 from openapi_server.models.products_response import ProductsResponse
 from openapi_server.models.security_controller_login200_response import SecurityControllerLogin200Response
 from openapi_server.models.security_controller_login_request import SecurityControllerLoginRequest
 from openapi_server.models.settings import Settings
+from openapi_server.models.successful_response import SuccessfulResponse
+from openapi_server.models.too_many_requests import TooManyRequests
+from openapi_server.models.unathorized import Unathorized
+from openapi_server.models.updated import Updated
 from openapi_server.models.user import User
 from openapi_server.models.user_response import UserResponse
