2100d86061ae768854bbace7cfe1ac29790f37af
Mallo321123 <mario@binary-kitchen.de>
2024-12-31 15:34:24 +0100

    advance sql inject function

--- backend/openapi_server/security.py
+++ backend/openapi_server/security.py
@@ -1,21 +1,30 @@
 import re
+import unicodedata
+
+
+def normalize_input(input_value: str) -> str:
+    return unicodedata.normalize("NFKC", input_value)
 
 
 # Check if the value contains any SQL injection keywords
 def check_sql_inject_value(value: str) -> bool:
-    
-    
-    SQL_INJECTION_REGEX = re.compile(
-        r"[;'\"--]|(\b(SELECT|DROP|INSERT|DELETE|UPDATE|UNION|OR|AND)\b)", re.IGNORECASE
-    )
-    
-    if value is not None and SQL_INJECTION_REGEX.search(value):
-        return False
-    else:
+    if not isinstance(value, str):
         return True
 
+    value = normalize_input(value)
+
+    sql_injection_regex = re.compile(
+        r"""
+        [;'"--]|
+        (\b(SELECT|DROP|INSERT|DELETE|UPDATE|UNION|OR|AND)\b)  # SQL-keywords
+    """,
+        re.IGNORECASE | re.VERBOSE,
+    )
+
+    return not bool(sql_injection_regex.search(value))
+
+
 def check_sql_inject_json(**kwargs) -> bool:
-    
     for key, value in kwargs.items():
         if not check_sql_inject_value(value):
             print(f"Unsichere Eingabe erkannt: {key} = {value}")
